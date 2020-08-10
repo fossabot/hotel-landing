dff0fb116dd7f0f71003ff41a10bc408ed825263
Olga <obulat@gmail.com>
2020-08-10 17:48:28 +0300

    Add checkbox-based elements
    Signed-off-by: Olga <obulat@gmail.com>

---
+++ src/blocks/checkbox/checkbox.js
@@ -0,0 +1,5 @@
+
+const checkbox = document.querySelector(".checkbox-button__input");
+checkbox.addEventListener('change', function() {
+    console.log("Checked: ", checkbox.checked);
+})
--- src/pages/ui_kit/ui_kit.js
+++ src/pages/ui_kit/ui_kit.js
@@ -1,3 +1,8 @@
 import "../../styles/fonts/_fonts.scss";
 import "./ui_kit.scss";
 import "../../blocks/dropdown/dropdown";
+import "../../blocks/checkbox/checkbox";
+import "../../blocks/radiobutton/radiobutton";
+import "../../blocks/rate-button/rate-button";
+import "../../blocks/toggle/toggle";
+
