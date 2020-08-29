37f0aaad05be60003111ae4493e9c2dda167a82b
Olga <obulat@gmail.com>
2020-08-29 18:09:29 +0300

    Make search_room page pixel-perfect
    Signed-off-by: Olga <obulat@gmail.com>

--- src/pages/search_room/search_room.js
+++ src/pages/search_room/search_room.js
@@ -1,3 +1,4 @@
 import '../../styles/fonts/_fonts.scss';
 import './search_room.scss';
 import '../../blocks/expandable-checkbox-list/expandable-checkbox-list';
+import '../../blocks/slider/slider';
