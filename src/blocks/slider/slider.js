bb0948cc6ab4bc94ad0c8f6902de55150866eacf
Olga <obulat@gmail.com>
2020-09-16 23:19:46 +0300

    Refactor slider to use a class
    Signed-off-by: Olga <obulat@gmail.com>

--- src/blocks/slider/slider.js
+++ src/blocks/slider/slider.js
@@ -1,12 +1,20 @@
+/* eslint-disable no-console */
 import 'nouislider/distribute/nouislider.css';
 import noUiSlider from 'nouislider';
 
-const slider = document.querySelector('.js-slider');
-noUiSlider.create(slider, {
+const defaultOptions = {
   start: [5000, 10000],
   connect: true,
   range: {
     min: 500,
     max: 15500,
   },
-});
+};
+
+class RangeSlider {
+  constructor(element, options) {
+    this.options = options || defaultOptions;
+    this.slider = noUiSlider.create(element, this.options);
+  }
+}
+export default RangeSlider;
