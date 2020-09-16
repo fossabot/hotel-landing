71c328c32d74ddf275d368cfc88ad0c7cf634988
Olga <obulat@gmail.com>
2020-09-16 23:17:50 +0300

    Replace datepicker with flatpickr
    Signed-off-by: Olga <obulat@gmail.com>

--- src/blocks/date-filter/date-filter.js
+++ src/blocks/date-filter/date-filter.js
@@ -1,26 +1,104 @@
-/* eslint-disable import/no-unresolved */
-import { DateRangePicker } from 'vanillajs-datepicker';
-import Datepicker from 'vanillajs-datepicker/js/Datepicker';
-import ru from '../../../node_modules/vanillajs-datepicker/js/i18n/locales/ru';
+/* eslint-disable no-param-reassign */
 
-Object.assign(Datepicker.locales, ru);
+import flatpickr from 'flatpickr';
+import rangePlugin from 'flatpickr/dist/plugins/rangePlugin';
+import { Russian } from 'flatpickr/dist/l10n/ru';
+import './theme.css';
+import 'flatpickr/dist/flatpickr.min.css';
 
-const isRange = (calendar) => calendar.querySelector('.start').length > 0;
-const DateFilter = [];
-
-const calendar = document.querySelector('.js-dates');
-const options = {
-  language: 'ru',
-  container: '.js-dates',
-  buttonClass: 'date-filter__button',
-  nextArrow: '<i class="material-icons datepicker-icon">arrow_forward</i>',
-  prevArrow: '<i class="material-icons datepicker-icon">arrow_back</i>',
-  todayHighlight: true,
-  weekStart: 1,
+const addClasses = (element, classes) => {
+  classes.split(' ').forEach((c) => {
+    element.classList.add(c);
+  });
 };
 
-if (isRange) {
-  DateFilter.push(new DateRangePicker(calendar, options));
+const storeDates = (dates) => {
+  window.localStorage.setItem('toxin_dates', JSON.stringify(dates));
+};
+
+/* eslint-disable consistent-return */
+const getStoredDates = () => {
+  const datesString = window.localStorage.getItem('toxin_dates');
+  if (datesString) {
+    const datesArr = JSON.parse(datesString);
+    return datesArr.map((d) => new Date(d));
+  }
+};
+const clearStoredDates = () => {
+  window.localStorage.removeItem('toxin_dates');
+};
+
+const addClearApply = (selectedDates, dateStr, instance) => {
+  const buttons = document.createElement('div');
+  addClasses(buttons, 'date-filter__buttons js-buttons');
+  const clearDates = () => {
+    instance.clear();
+    clearStoredDates();
+  };
+  const applyDates = () => {
+    instance.close();
+    storeDates(instance.selectedDates);
+  };
+  const clearButton = document.createElement('button');
+  addClasses(clearButton, 'button button_text button_gray');
+  clearButton.textContent = 'Очистить';
+  clearButton.addEventListener('click', clearDates);
+
+  const applyButton = document.createElement('button');
+  addClasses(applyButton, 'button button_text');
+  applyButton.textContent = 'Применить';
+  applyButton.addEventListener('click', applyDates);
+
+  buttons.appendChild(clearButton);
+  buttons.appendChild(applyButton);
+
+  instance.calendarContainer.appendChild(buttons);
+};
+
+const defaultConfig = {
+  locale: Russian,
+  nextArrow: '<i class="material-icons datepicker-icon">arrow_forward</i>',
+  prevArrow: '<i class="material-icons datepicker-icon">arrow_back</i>',
+  weekStart: 1,
+  mode: 'range',
+  dateFormat: 'j M',
+  plugins: [],
+  closeOnSelect: false,
+  onReady: [addClearApply],
+  minDate: new Date(),
+};
+
+class DateFilter {
+  constructor(element) {
+    this.element = element;
+    this.initPickers();
+  }
+
+  initPickers() {
+    const config = { ...defaultConfig };
+    const defaultDates = getStoredDates();
+    if (defaultDates) {
+      config.defaultDate = defaultDates;
+    }
+    const inputs = this.element.querySelectorAll('input');
+    this.hasTwoInputs = inputs.length === 2;
+    if (this.hasTwoInputs) {
+      /* eslint-disable new-cap */
+      this.picker = flatpickr(
+        inputs[0],
+        {
+          ...config,
+          plugins: [
+            ...config.plugins,
+            new rangePlugin({ input: inputs[1] }),
+          ],
+        },
+      );
+    } else {
+      config.wrap = true;
+      this.picker = flatpickr(this.element, config);
+    }
+  }
 }
 
 export default DateFilter;
