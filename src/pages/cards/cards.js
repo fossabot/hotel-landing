889218f3f90f78f6b72c18937942f660d7c7f567
Olga <obulat@gmail.com>
2020-09-17 22:36:50 +0300

    Extract cards from ui_kit to a separate page
    Signed-off-by: Olga <obulat@gmail.com>

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -1,2 +1,13 @@
+/* eslint-disable no-console */
 import '../../styles/fonts/_fonts.scss';
 import './cards.scss';
+import DateFilter from '../../blocks/date-filter/date-filter';
+
+function initAll() {
+  const calendars = document.querySelectorAll('.js-calendar');
+  console.log('Initializing, ', calendars);
+  const isCalendarInline = true;
+  calendars.forEach((calendar) => new DateFilter(calendar, isCalendarInline));
+}
+
+document.addEventListener('DOMContentLoaded', initAll);
