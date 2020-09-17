c2b3edae295b5ecd74f5963417a26bef839c5c31
Olga <obulat@gmail.com>
2020-09-17 22:29:42 +0300

    Fix Perfect-Pixel problems in search_room page, fix RangeSlider initialization
    Signed-off-by: Olga <obulat@gmail.com>

--- src/pages/search_room/search_room.js
+++ src/pages/search_room/search_room.js
@@ -1,4 +1,11 @@
 import '../../styles/fonts/_fonts.scss';
 import './search_room.scss';
 import '../../blocks/expandable-checkbox-list/expandable-checkbox-list';
-import '../../blocks/slider/slider';
+import RangeSlider from '../../blocks/slider/slider';
+
+const initAll = () => {
+  const sliders = document.querySelectorAll('.js-slider');
+  sliders.forEach((slider) => new RangeSlider(slider));
+};
+
+document.addEventListener('DOMContentLoaded', initAll);
